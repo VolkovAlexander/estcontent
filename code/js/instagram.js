898e23e3d726c00ef632ab2e49a51ae6ee2a21d4
Alexander Volkov <volkkb@gmail.com>
2019-11-27 06:08:52 +0500

    Доп. правки

--- code/js/instagram.js
+++ code/js/instagram.js
@@ -8,14 +8,19 @@ function parseUsers()
         'candidates': candidates
     }, function(response) {
         response = JSON.parse(response);
-        total_count = response.total;
-        list = response.list;
 
-        $(document).find('#firstPart').hide();
+        if(parseInt(response.total) <= 8) {
+            alert('Недостаточное количество участников! Проверьте вводимые данные');
+        } else {
+            total_count = response.total;
+            list = response.list;
 
-        $(document).find('#totalCount').html(total_count);
-        $(document).find('#list').val(JSON.stringify(list));
-        $(document).find('#secondPart').show();
+            $(document).find('#firstPart').hide();
+
+            $(document).find('#totalCount').html(total_count);
+            $(document).find('#list').val(JSON.stringify(list));
+            $(document).find('#secondPart').show();
+        }
     });
 }
 
@@ -35,7 +40,6 @@ function findRandomUser()
 
         randomNumber = parseInt(Math.random() * max);
         for(var i = 0; i < oldVal.length; i++) {
-            console.log(oldVal[i]);
             if(parseInt(oldVal[i].id) === parseInt(randomNumber)) {
                 finished = false;
             }
@@ -50,12 +54,16 @@ function findRandomUser()
     $.post( "instagram.php?account=" + list[randomNumber], function(response) {
         response = JSON.parse(response);
 
-        $(document).find('#winners').val(JSON.stringify(oldVal.concat([{
-            'id': randomNumber,
-            'picture': response.picture,
-            'full_name': response.full_name,
-            'username': list[randomNumber]
-        }])));
+        if(response.picture === null || response.full_name === null) {
+            alert('Учетная запись победителя не найдена в Инстаграме. Повторите попытку');
+        } else {
+            $(document).find('#winners').val(JSON.stringify(oldVal.concat([{
+                'id': randomNumber,
+                'picture': response.picture,
+                'full_name': response.full_name,
+                'username': list[randomNumber]
+            }])));
+        }
 
         updateWinners();
     });
@@ -64,27 +72,32 @@ function findRandomUser()
 function updateWinners()
 {
     let winners = $(document).find('#winners').val();
-    winners = JSON.parse(winners);
+    winners = winners.length ? JSON.parse(winners) : [];
 
-    if(winners.length === 8) {
-        $(document).find('#searchButton').hide();
+    if(!winners.length) {
+        $(document).find('#bigWinnerBlock').css('display', 'none');
+        $(document).find('#waiterBlock').css('display', 'none');
     } else {
-        $(document).find('#searchButton').removeAttr('disabled');
-    }
+        if(winners.length === 8) {
+            $(document).find('#searchButton').hide();
+        } else {
+            $(document).find('#searchButton').removeAttr('disabled');
+        }
 
-    for(var i = 0; i < 8; i++) {
-        if(typeof(winners[i]) !== 'undefined') {
-            $(document).find('#winnerSmallImage_' + i).attr('src', winners[i].picture);
-            $(document).find('#winnerSmallLink_' + i).attr('href', 'https://instagram.com/' + winners[i].username);
+        for(var i = 0; i < 8; i++) {
+            if(typeof(winners[i]) !== 'undefined') {
+                $(document).find('#winnerSmallImage_' + i).attr('src', winners[i].picture);
+                $(document).find('#winnerSmallLink_' + i).attr('href', 'https://instagram.com/' + winners[i].username);
 
-            if(i === (winners.length - 1)) {
-                $(document).find('#winnerBigImage').attr('src', winners[i].picture);
-                $(document).find('#winnerBigName').html(winners[i].full_name);
-                $(document).find('#winnerBigLink').html('https://instagram.com/' + winners[i].username);
-                $(document).find('#winnerBigLink').attr('href', 'https://instagram.com/' + winners[i].username);
+                if(i === (winners.length - 1)) {
+                    $(document).find('#winnerBigImage').attr('src', winners[i].picture);
+                    $(document).find('#winnerBigName').html(winners[i].full_name);
+                    $(document).find('#winnerBigLink').html('https://instagram.com/' + winners[i].username);
+                    $(document).find('#winnerBigLink').attr('href', 'https://instagram.com/' + winners[i].username);
 
-                $(document).find('#bigWinnerBlock').css('display', 'block');
-                $(document).find('#waiterBlock').css('display', 'none');
+                    $(document).find('#bigWinnerBlock').css('display', 'block');
+                    $(document).find('#waiterBlock').css('display', 'none');
+                }
             }
         }
     }
--- code/parse.php
+++ code/parse.php
@@ -3,7 +3,17 @@
 $candidates = $_POST['candidates'];
 $candidates = explode("\n", $candidates);
 
+if(!empty($candidates)) {
+    $candidates = array_unique($candidates);
+
+    foreach ($candidates as $index => $candidate) {
+        if(strlen($candidate) === 0) {
+            unset($candidates[$index]);
+        }
+    }
+}
+
 echo json_encode([
     'total' => count($candidates),
-    'list' => $candidates
+    'list' => array_values($candidates)
 ]);
